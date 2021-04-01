2cbb9c0857ffd10f2fe503f7b17e1ebc589f81a6
HillbillyZT <chris30257@gmail.com>
2021-04-01 18:11:14 -0500

    SHIT WORKS

--- Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/Instruction_Decoder.v
+++ Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/Instruction_Decoder.v
@@ -1,42 +1,32 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/25/2021 01:49:48 PM
-// Design Name: 
-// Module Name: Instruction_Decoder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module Instuction_Decoder(
+	/*input [31:0] instruction,
+	output[6:0] opcode,
+	output RW,
+	output[1:0] MD,
+	output[1:0] BS,
+	output PS,
+	output MW,
+	output[4:0] FS,
+	output MA,
+	output MB,
+	output[4:0] AA,
+	output[4:0] BA,
+	output CS*/
+	
+	//Not 100% sure about what goes where.
+);
+//takes in opcode, decides everything based on that.
+assign opcode = instruction[31:25];
+assign RW = instruction[24];
+assign MD = instruction[23:22];
+assign BS = instruction[21:20];
+assign PS = instruction[19];
+assign MW = instruction[18];
+assign FS = instruction[17:13];
+assign MA = instruction[12];
+assign MB = instruction[11];
+assign AA = instruction[10:6];
+assign BA = instruction[5:1];
+assign CS = instruction[0];
 
-
-module Instruction_Decoder(
-    input[31:0] instruction,
-    output[6:0] opcode,
-    output RW,
-    output[1:0] MD,
-    output[1:0] BS,
-    output PS,
-    output MW,
-    output[4:0] FS,
-    output MB,
-    output MA,
-    output CS
-    );
-    assign opcode = instruction[31:25];
-    assign RW = instruction[24];
-    //assign DA = instruction[something];
-    //assign AA = instruction[something];
-    //assign BA = instruction[something];
-    assign CS = instruction[0];
 endmodule
