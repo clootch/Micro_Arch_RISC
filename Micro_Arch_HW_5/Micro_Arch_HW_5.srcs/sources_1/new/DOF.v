42af82f1120cd530eeb99b9f3d45906d32cb765e
baileycarothers <44585056+baileycarothers@users.noreply.github.com>
2021-04-01 18:16:14 -0500

    Update
    Removed useless descriptions

--- Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/DOF.v
+++ Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/DOF.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/01/2021 04:27:48 PM
-// Design Name: 
-// Module Name: DOF
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module DOF(
     input[31:0] IR,
