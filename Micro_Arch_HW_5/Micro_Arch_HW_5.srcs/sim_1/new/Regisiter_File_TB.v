90edf1171baa27de279643f915a7838e1d20a8bc
HillbillyZT <chris30257@gmail.com>
2021-04-01 19:10:18 -0500

    register TB does smth now

--- Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sim_1/new/Regisiter_File_TB.v
+++ Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sim_1/new/Regisiter_File_TB.v
@@ -1,26 +1,6 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/28/2021 02:03:57 PM
-// Design Name: 
-// Module Name: Regisiter_File_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module Regisiter_File_TB();
+module Register_File_TB();
     reg[4:0] AA;
     reg[4:0] BA;
     reg[4:0] DA;
@@ -51,7 +31,7 @@ module Regisiter_File_TB();
         RW = 1;
         DA = 4;
         #10;
-        AA = 5'd2;
+        AA = 4;
         BA = 5'd5;
     end
     always
