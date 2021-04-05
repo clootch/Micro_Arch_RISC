4ebcfe19585060d1f24fd424ecd0c1b61dbbaad6
HillbillyZT <chris30257@gmail.com>
2021-04-04 22:33:39 -0500

    "finish" WB, add EX and Top to proj

--- Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/WB.v
+++ Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/WB.v
@@ -7,17 +7,18 @@ module WB(
     input [31:0] F,
     input NV,
     input [31:0] DATA,
-    input clk
+    input clk,
+    output RW_o,
+    output DA_o,
+    output [31:0] BUS_D
     );
     
-    wire [31:0] BUS_D;
+    
+    assign RW_o = RW;
+    assign DA_o = DA;
     
     assign BUS_D = (MD==2'b00) ? 
         F : (MD==2'b01) ? 
             DATA : 32'b0;
-    
-    always @ (posedge clk) begin
-                
-    end
     
 endmodule
