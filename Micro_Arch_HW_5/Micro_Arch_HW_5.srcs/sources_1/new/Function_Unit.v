54bd02ae6591655d39d11141f446430c122dc055
HillbillyZT <chris30257@gmail.com>
2021-04-06 18:42:53 -0500

    function unit fix

--- Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/Function_Unit.v
+++ Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/Function_Unit.v
@@ -27,60 +27,60 @@ always @(*)
 begin
     direction = ~FS[1:0];
 	case(FS)
-		5'b00000: begin
-		F = A;
-		V = 0;
+		5'b00000: begin //pass A
+			F = A;
+			V = 0;
 		end
-		5'b00010: begin
-		{C,F} = {1'b0,A} + {1'b0,B};
-		V = (A[31] == B[31]) && (A[31] != F[31]);
+		5'b00010: begin //add
+			{C,F} = A+B;
+			V = (A[31] == B[31]) && (A[31] != F[31]);
 		end
-		5'b00101: begin
-		{C,F} = {1'b0,A} + {1'b0,B} + 1; //Need a special case here (Refer to Table 10-20)
-		V = (A[31] == B[31]) && (A[31] != F[31]);
+		5'b00101: begin //sub
+			{C,F} = A + ~B + 1; //Need a special case here (Refer to Table 10-20)
+			V = (A[31] != B[31]) && (F[31] == B[31]);
 		end
-		5'b01000: begin
-		F = A & B;
-		V =0;
+		5'b01000: begin //AND
+			F = A & B;
+			C = 0;
+			V =0;
 		end
-		5'b01010: begin
-		F = A | B;
-		V =0;
+		5'b01010: begin //OR
+			F = A | B;
+			C = 0;
+			V =0;
 		end
-		5'b01100: begin
-		F = A ^ B;
-		V =0;
+		5'b01100: begin //XOR
+			F = A ^ B;
+			C = 0;
+			V =0;
 		end
-		5'b01110: begin
-		F = ~A;
-		V =0;
+		5'b01110: begin //!A
+			F = ~A;
+			C = 0;
+			V =0;
 		end
-		5'b00111: begin
-		{C,F} = {1'b0,A} + 1;
-		V = F[31] != A[31] && A[31] == 0;
+		5'b00111: begin //inc A
+			{C,F} = A + 1;
+			V = F[31] != A[31] && A[31] == 0;
 		end
-		5'b10000: begin
-		F = shiftval; //left
-		V =0;
+		5'b10000: begin //LSL
+			F = shiftval; //left
+			V =0;
 		end
-		5'b10001: begin
-		F = shiftval; //right
-		V =0;
+		5'b10001: begin //LSR
+			F = shiftval; //right
+			V =0;
 		end
 		default: begin
-		F = 1'b0;
-		V =0;
+			F = 1'b0;
+			V =0;
 		end
 	endcase
+	
 	//Zero
-	if (F == 0)
-	begin
-		Z = 0;
-	end
+	if (F == 0) Z = 0;
+	
 	//Negative 
-	if(F[31] == 1)
-	begin
-		N = F[31];
-	end
+	N = F[31];
 end
 endmodule
