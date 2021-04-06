0fd39dd5c883e650c33ea0f5471fafbf13b9ee12
clootch <68715725+clootch@users.noreply.github.com>
2021-04-06 14:22:58 -0500

    WB TB

--- OLD STUFF DONT TOUCH/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/DOF_TB_behav/webtalk/xsim_webtalk.tcl
+++ OLD STUFF DONT TOUCH/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/DOF_TB_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/Github Repos/Micro_Arch_RISC/OLD STUFF DONT TOUCH/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/DOF_TB_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sun Apr  4 23:09:17 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Apr  6 11:08:55 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "a7dfe7c8-e032-4b6a-9164-18956ff165de" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "6ac85d3e3d274f30baea35af15bffc6b" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "18" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "32" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-8750H CPU @ 2.20GHz" -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "17.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "250 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6416_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 728022509 -regid "" -xml D:/Github Repos/Micro_Arch_RISC/OLD STUFF DONT TOUCH/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/DOF_TB_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Github Repos/Micro_Arch_RISC/OLD STUFF DONT TOUCH/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/DOF_TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Github Repos/Micro_Arch_RISC/OLD STUFF DONT TOUCH/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/DOF_TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6612_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2230836751 -regid "" -xml D:/Github Repos/Micro_Arch_RISC/OLD STUFF DONT TOUCH/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/DOF_TB_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Github Repos/Micro_Arch_RISC/OLD STUFF DONT TOUCH/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/DOF_TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Github Repos/Micro_Arch_RISC/OLD STUFF DONT TOUCH/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/DOF_TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
