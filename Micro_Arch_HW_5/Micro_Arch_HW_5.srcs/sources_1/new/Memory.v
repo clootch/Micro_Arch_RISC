79c2616290c6b91b7cdaa07f63d7e8e46c3e03a5
HillbillyZT <chris30257@gmail.com>
2021-04-06 16:57:37 -0500

    Finish Memory.v

--- Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/Memory.v
+++ Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/Memory.v
@@ -1,37 +1,28 @@
 module Memory(
     input clk,
-	input reset,
     input A,
+    input B,
     //input Bus_A,
     //input Bus_B,
     //input [9:0] address_in,
-    output MW,
-    output reg [31:0] data_out
+    input MW,
+    output [31:0] data_out
 );
 
-    reg [31:0] memory [1024:0];
+    reg [31:0] memory [1023:0];
     integer i;
 
     //Initialize everything to 0
     initial begin
-        for(i=0; i<1023; i=i+1)
+        for(i=0; i<1024; i=i+1)
         begin
             memory[i] = 32'd0;
         end
-        data_out = 32'd0;
+        //data_out = 32'd0;
     end
 
-    always @ (negedge clk)
-    begin
-        if(!reset)
-        begin
-            //Do stuff
-        end else begin
-            //Same loop as initial begin, set everything to 0
-            for(i=0; i<1023; i=i+1)
-            begin
-                memory[i] = 32'd0;
-            end
-        end
-    end
+    assign data_out = memory[A];
+
+    always @(negedge clk) if(MW) memory[A] = B;
+
 endmodule
