6f6a08e02c067aa407ce8ea3350b9e7e2094e881
HillbillyZT <chris30257@gmail.com>
2021-04-06 20:50:40 -0500

    WB and WB_TB

--- Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sim_1/new/WB_TB.v
+++ Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sim_1/new/WB_TB.v
@@ -5,21 +5,29 @@ module WB_TB();
     reg [4:0] DA;
     reg [1:0] MD;
     reg [31:0] F, DATA;
+    
+    wire [4:0] DA_o;
+    wire [31:0] BUS_D;
+    wire RW_o;
+    
     WB uut(
-    .RW(RW),
-    .DA(DA),
-    .MD(MD),
-    .F(F),
-    .NV(NV),
-    .DATA(DATA),
-    .clk(clk)
-);
+        .RW(RW),
+        .DA(DA),
+        .MD(MD),
+        .F(F),
+        .NV(NV),
+        .DATA(DATA),
+        .clk(clk),
+        .DA_o(DA_o),
+        .RW_o(RW_o),
+        .BUS_D(BUS_D)
+    );
 
     Register_File RF(
-            .DA(DA),
+            .DA(DA_o),
             .clk(clk),
-            .RW(RW),
-            .D(D)
+            .RW(RW_o),
+            .D(BUS_D)
         );
 
     initial begin
@@ -27,7 +35,24 @@ module WB_TB();
         DATA = 32'd123;
         DA = 32'd4;
         NV = 0;
+        F = 0;
+    end
+    
+    reg [31:0] count = 0;
+    
+    always @(negedge clk) begin
+        if(count == 1) begin
+            MD = 1;
+            RW = 1;
+            DA = 1;
+            F = 11;
+            NV = 0;
+            
+        end
         
+        
+        
+        count = count + 1;
     end
 
     always #5 clk = ~clk;
--- Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/WB.v
+++ Micro_Arch_HW_5/Micro_Arch_HW_5.srcs/sources_1/new/WB.v
@@ -2,22 +2,21 @@
 
 module WB(
     input RW,
-    input DA,
+    input [4:0] DA,
     input [1:0] MD,
     input [31:0] F,
     input NV,
     input [31:0] DATA,
-    input clk
+    input clk,
+    output [4:0] DA_o,
+    output RW_o,
+    output [31:0] BUS_D
     );
-    
-    wire [31:0] BUS_D;
     
     assign BUS_D = (MD==2'b00) ? 
         F : (MD==2'b01) ? 
             DATA : {31'b0, NV};
     
-    always @ (posedge clk) begin
-                
-    end
-    
+    assign DA_o = DA;
+    assign RW_o = RW;
 endmodule
